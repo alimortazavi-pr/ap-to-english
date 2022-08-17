7bf412d60c82d328c192af9c6ebedbda7274ba2c
Ali Mortazavi <mortazavimaneshali@gmail.com>
2022-08-17 16:35:20 +0430

    V 0.0.1

---
+++ README.md
@@ -0,0 +1,20 @@
+### You can convert persian or arabic number to english number
+
+```js
+// install with npm 
+npm install ap-to-english
+```
+
+## Usage
+
+```js
+import convertToEnglish from 'num-to-english';
+
+//Convert
+convertToEnglish('۱۳۴۵۶۷۸' or '١٢٣٤٥٦٧٨'); // 12345678
+```
+
+## Features
+
+- Very fast
+- Very easy
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+//Convert numbers
+function convertAPToEnglish(number: number | string): string {
+  try {
+    return number
+      .toString()
+      .replace(/[٠-٩]/g, (d): string => "٠١٢٣٤٥٦٧٨٩".indexOf(d).toString())
+      .replace(/[۰-۹]/g, (d) => "۰۱۲۳۴۵۶۷۸۹".indexOf(d).toString());
+  } catch (error) {
+    throw new Error("Please enter a correct number");
+  }
+}
+
+export default convertAPToEnglish;
